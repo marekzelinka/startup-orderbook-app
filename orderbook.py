f70c764a9b0193376a806e240443790a0277b3a0
Marek Zelinka <mzelinka17@gmail.com>
2025-12-08 18:52:50 +0100

    cleanup

--- orderbook.py
+++ orderbook.py
@@ -5,12 +5,6 @@ class Orderbook:
     def __init__(self) -> None:
         self.__tasks: dict[int, Task] = {}
 
-    def add_order(self, description: str, programmer: str, workload: int) -> None:
-        new_id = len(self.__tasks) + 1
-        task = Task(new_id, description, programmer, workload)
-
-        self.__tasks[task.id] = task
-
     @property
     def orders(self) -> list[Task]:
         return list(self.__tasks.values())
@@ -19,14 +13,6 @@ class Orderbook:
     def programmers(self) -> list[str]:
         return list(set(task.programmer for task in self.__tasks.values()))
 
-    def mark_order_finished(self, order_id: int) -> None:
-        task = self.__tasks.get(order_id)
-
-        if not task:
-            raise ValueError(f"Task with id of {order_id} not found")
-
-        task.mark_finished()
-
     @property
     def finished_orders(self) -> list[Task]:
         return [task for task in self.__tasks.values() if task.finished]
@@ -34,6 +20,20 @@ class Orderbook:
     @property
     def unfinished_orders(self) -> list[Task]:
         return [task for task in self.__tasks.values() if not task.finished]
+
+    def add_order(self, description: str, programmer: str, workload: int) -> None:
+        new_id = len(self.__tasks) + 1
+        task = Task(new_id, description, programmer, workload)
+
+        self.__tasks[task.id] = task
+
+    def mark_order_finished(self, order_id: int) -> None:
+        task = self.__tasks.get(order_id)
+
+        if not task:
+            raise ValueError(f"Task with id of {order_id} not found")
+
+        task.mark_finished()
 
     def status_of_programmer(self, programmer: str) -> tuple[int, int, int, int]:
         assignet_tasks = [
